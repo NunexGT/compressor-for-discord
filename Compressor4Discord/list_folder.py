035977af8b0028195f9182df9716d74e96353d0e
SuperX-dev <74361788+SuperX-dev@users.noreply.github.com>
2022-03-09 17:31:16 +0000

    fold lister

--- Compressor4Discord/list_folder.py
+++ Compressor4Discord/list_folder.py
@@ -1,9 +1,29 @@
 import os
+from statistics import median
 
-extensions = open("file_extensions.list").read().splitlines()
-print(extensions)
+def list_folder(mediadir):
+  extensions_file = open("file_extensions.list", "r")
 
-def list_media_folder(mediadir):
+  extensions_list = []
+  for line in extensions_file:
+    stripped_line = line.strip()
+    line_list = stripped_line.split()
+    extensions_list.append(line_list)
+
+  extensions_file.close()
+  loop_int = -1
+
+  def check_files_with_extension(extensions_list,loop_int):
+    for x in extensions_list:
+        loop_int = loop_int + 1
+        extension__list_value = extensions_list[loop_int]
+        extension_str = str(extension__list_value)[1:-1]
+        extension = extension_str.replace("'",'',2)
+        get_files(extension)
+
+  def get_files(extension):
     for file in os.listdir(mediadir):
-        if file.endswith(extensions):
-         print(os.path.join(mediadir, file))
+      if file.endswith(extension):
+        print(os.path.join(mediadir, file))
+
+  check_files_with_extension(extensions_list,loop_int)
